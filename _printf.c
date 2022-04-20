3dd2cb9728e94e9033f07ccf8c17f36b389aa1e8
Godwin Martins <gmcodes@gmail.com>
2022-04-21 00:50:02 +0100

    Changes to main

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
+ */
+int _printf(const char *format, ...)
+{
+	
+}
