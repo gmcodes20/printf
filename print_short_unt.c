75e8cd4b96c91328be6cfe9fb3870e0e8206d60d
Joshua Ajayi <Joshayotundeaj@gmail.com>
2022-04-22 16:34:14 +0000

    Update print_short_unt.c

--- print_short_unt.c
+++ print_short_unt.c
@@ -9,6 +9,22 @@
  */
 int prinhunt(va_list arguments, char *buf, unsigned int ibuf)
 {
+	unsigned short int int_in, int_temp, i, div;
 
+	int_in = va_arg(arguments, unsigned int);
 
+	int_temp = int_in;
+	div = 1;
+
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i);
 }
