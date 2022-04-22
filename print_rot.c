c2086f9f12297035d08e521ee3f22a9dd32e8a74
Godwin Martins <gmcodes@gmail.com>
2022-04-22 09:24:05 +0100

    Create print_rot.c

---
+++ print_rot.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_rot - writes the str in ROT13
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+
+int print_rot(va_list arguments, char *buf, unsigned int ibuf)
+{
+
+}
