48524edbe9d368af2da525cb3535776463bc3588
Godwin Martins <gmcodes@gmail.com>
2022-04-22 08:52:06 +0100

    Create fill_long_oct_array.c

---
+++ fill_long_oct_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * fill_long_oct_array - calculates a long octal number
+ *
+ * @bnr: array where is stored the binary.
+ * @oct: array where is stored the octal.
+ *
+ * Return: binary array.
+ */
+char *fill_long_oct_array(char *bnr, char *oct)
+{
+
+}
