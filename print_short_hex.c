32a0d63a77c4829168e7749f3eaea0300b526660
Godwin Martins <gmcodes@gmail.com>
2022-04-22 09:21:46 +0100

    Create  print_short_hex.c

---
+++ print_short_hex.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * prinhhex - prints a short decimal in hexadecimal
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int prinhhex(va_list arguments, char *buf, unsigned int ibuf)
+{
+
+}
