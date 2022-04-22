7dc822bc6391b513aa40c34d8dd23f702bc20b83
Joshua Ajayi <Joshayotundeaj@gmail.com>
2022-04-22 15:54:32 +0000

    Update fill_hex_array.c

--- fill_hex_array.c
+++ fill_hex_array.c
@@ -12,5 +12,22 @@
  */
 char *fill_hex_array(char *bnr, char *hex, int isupp, int limit)
 {
+	int op, i, j, toletter;
 
+	hex[limit] = '\0';
+	if (isupp)
+		toletter = 55;
+	else
+		toletter = 87;
+	for (i = (limit * 4) - 1; i >= 0; i--)
+	{
+		for (op = 0, j = 1; j <= 8; j *= 2, i--)
+			op = ((bnr[i] - '0') * j) + op;
+		i++;
+		if (op < 10)
+			hex[i / 4] = op + 48;
+		else
+			hex[i / 4] = op + toletter;
+	}
+	return (hex);
 }
