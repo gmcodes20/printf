57e4e0c976adf62873dde388960cd9278ea63a67
Godwin Martins <gmcodes@gmail.com>
2022-04-22 09:30:12 +0100

    Create print_num_oct.c

---
+++ print_num_oct.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * prinnoct - print the number in octal begining with zero
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int prinnoct(va_list arguments, char *buf, unsigned int ibuf)
+{
+
+}
