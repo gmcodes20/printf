7274f998da518901d6f39e877e6256e96ae42582
Joshua Ajayi <Joshayotundeaj@gmail.com>
2022-04-22 16:14:02 +0000

    Update print_long_unt.c

--- print_long_unt.c
+++ print_long_unt.c
@@ -8,4 +8,19 @@
  */
 int prinlunt(va_list arguments, char *buf, unsigned int ibuf)
 {
+	unsigned long int int_in, int_temp, i, div;
+
+	int_in = va_arg(arguments, unsigned long int);
+	int_temp = int_in;
+	div = 1;
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i);
 }
