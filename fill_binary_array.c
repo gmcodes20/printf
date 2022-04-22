fbce4b98565e96eb1be62e5806da2214b4384f43
Joshua Ajayi <Joshayotundeaj@gmail.com>
2022-04-22 15:52:49 +0000

    Update fill_binary_array.c

--- fill_binary_array.c
+++ fill_binary_array.c
@@ -10,5 +10,28 @@
  */
 char *fill_binary_array(char *binary, long int int_in, int isneg, int limit)
 {
-	
+	int i;
+
+	for (i = 0; i < limit; i++)
+		binary[i] = '0';
+	binary[limit] = '\0';
+	for (i = limit - 1; int_in > 1; i--)
+	{
+		if (int_in == 2)
+			binary[i] = '0';
+		else
+			binary[i] = (int_in % 2) + '0';
+		int_in /= 2;
+	}
+	if (int_in != 0)
+		binary[i] = '1';
+	if (isneg)
+	{
+		for (i = 0; binary[i]; i++)
+			if (binary[i] == '0')
+				binary[i] = '1';
+			else
+				binary[i] = '0';
+	}
+	return (binary);
 }
