fcf6c85464c1282d4086eff3b990b8d0745385d2
Godwin Martins <gmcodes@gmail.com>
2022-04-22 09:17:52 +0100

    Create print_short_unt.c

---
+++ print_short_unt.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * prinhunt - prints a short unsigned integer
+ * @arguments: number to print
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int prinhunt(va_list arguments, char *buf, unsigned int ibuf)
+{
+
+
+}
