bd8847f953dd1ab5cc893e318523c22c67df9d73
Godwin Martins <gmcodes@gmail.com>
2022-04-22 09:20:33 +0100

    Create  print_short_oct.c

---
+++ print_short_oct.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * prinhoct - prints long decimal number in octal
+ * @arguments: input number
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int prinhoct(va_list arguments, char *buf, unsigned int ibuf)
+{
+
+}
