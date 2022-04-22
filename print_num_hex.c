11d12a74acf689c8bd008b3d0f211e1184c34a97
Godwin Martins <gmcodes@gmail.com>
2022-04-22 09:31:08 +0100

    Create print_num_hex.c

---
+++ print_num_hex.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * prinnhex - print number in hex begining with zero
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int prinnhex(va_list arguments, char *buf, unsigned int ibuf)
+{
+}
