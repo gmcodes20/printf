3600b7c0c91c144edb0c57b5a2cc276910ba7905
Joshua Ajayi <Joshayotundeaj@gmail.com>
2022-04-22 22:09:05 +0000

    create _printf.c

--- main.h
+++ main.h
@@ -51,7 +51,7 @@ int prinsint(va_list arguments, char *buf, unsigned int ibuf);
 int (*get_print_func(const char *s, int index))(va_list, char *, unsigned int);
 int ev_print_func(const char *s, int index);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
-int _putchar(char *buf, unsigned int nbuf);
+int print_buf(char *buf, unsigned int nbuf);
 char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
 char *fill_oct_array(char *bnr, char *oct);
 char *fill_long_oct_array(char *bnr, char *oct);
