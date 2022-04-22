2acdb70f434e6e8fe7cecaa1953bca8443c22d8f
Godwin Martins <gmcodes@gmail.com>
2022-04-22 08:48:39 +0100

    Create  ev_print_func.c

---
+++ ev_print_func.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * ev_print_func - returns the amount of identifiers.
+ * @s: argument indentifier
+ * @index: index of argument identifier.
+ * Return: amount of identifiers.
+ */
+int ev_print_func(const char *s, int index)
+{
+
+}
