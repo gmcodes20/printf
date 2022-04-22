994141a0e0a43d726c6a43daea959aaf1d0f3364
Godwin Martins <gmcodes@gmail.com>
2022-04-22 09:27:38 +0100

    Create print_plus_int.c

---
+++ print_plus_int.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * prinpint - print integer with plus symbol
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int prinpint(va_list arguments, char *buf, unsigned int ibuf)
+{
+
+}
