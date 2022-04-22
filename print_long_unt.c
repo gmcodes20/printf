72499999c541e3760fa0f4e6b0bdaba261e224f2
Godwin Martins <gmcodes@gmail.com>
2022-04-22 09:33:09 +0100

    Create print_long_unt.c

---
+++ print_long_unt.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * prinlunt - prints a long unsigned integer
+ * @arguments: number to print
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int prinlunt(va_list arguments, char *buf, unsigned int ibuf)
+{
+}
