f42f845a102cb61a3dc5768929ba5655c67a1900
Joshua Ajayi <Joshayotundeaj@gmail.com>
2022-04-22 15:56:58 +0000

    Update fill_oct_array.c

--- fill_oct_array.c
+++ fill_oct_array.c
@@ -10,5 +10,19 @@
  */
 char *fill_oct_array(char *bnr, char *oct)
 {
+	int op, i, j, ioct, limit;
 
+	oct[11] = '\0';
+	for (i = 31, ioct = 10; i >= 0; i--, ioct--)
+	{
+		if (i > 1)
+			limit = 4;
+		else
+			limit = 2;
+		for (op = 0, j = 1; j <= limit; j *= 2, i--)
+			op = ((bnr[i] - '0') * j) + op;
+		i++;
+		oct[ioct] = op + '0';
+	}
+	return (oct);
 }
