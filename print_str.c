0b957e4f6dfe810f64aeb0befe78da06282ef86b
Godwin Martins <gmcodes@gmail.com>
2022-04-22 09:14:28 +0100

    Create print_str.c

---
+++ print_str.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_str - writes the string to stdout
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: On success 1.
+ */
+int print_str(va_list arguments, char *buf, unsigned int ibuf)
+{
+
+}
