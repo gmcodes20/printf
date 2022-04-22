42067f802fea5a8c490ac77760f34ffd11c894da
Godwin Martins <gmcodes@gmail.com>
2022-04-22 09:29:21 +0100

    Create print_num_upx.c

---
+++ print_num_upx.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * prinnupx - prints number in uppercase hex
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int prinnupx(va_list arguments, char *buf, unsigned int ibuf)
+{
+
+}
