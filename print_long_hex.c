8f5d93945cd6d3e7660b794b6e8dd1c1c9e923a6
Godwin Martins <gmcodes@gmail.com>
2022-04-22 09:09:30 +0100

    Create print_long_int.c  and print_long_hex.c

---
+++ print_long_hex.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * prinlhex - prints a long decimal in hexadecimal
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int prinlhex(va_list arguments, char *buf, unsigned int ibuf)
+{
+
+}
---
+++ print_long_int.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * prinlint - prints a long integer
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int prinlint(va_list arguments, char *buf, unsigned int ibuf)
+{
+
+}
